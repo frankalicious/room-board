6389184c9cf4fb1e4567463c3bb112d2270b0f44
dasdgw <michifrank2@gmx.de>
2014-11-09 13:47:10 +0100

    move submodules to subfolders (adc/uart/etc) and adjust makefile

--- firmware/msp430-gcc/uart/uart.c
+++ firmware/msp430-gcc/uart/uart.c
@@ -55,6 +55,7 @@ void uart_init(void)
   	UCA0CTL1 |= UCSSEL_2;                     /* SMCLK */
   	UCA0BR0 = 104;                            /* 1MHz 9600 */
   	UCA0BR1 = 0;                              /* 1MHz 9600 */
+//  	UCA0MCTL = UCBRS2;                        /* Modulation UCBRSx = 1 */
   	UCA0MCTL = UCBRS0;                        /* Modulation UCBRSx = 1 */
   	UCA0CTL1 &= ~UCSWRST;                     /* Initialize USCI state machine */
   	IE2 |= UCA0RXIE;                          /* Enable USCI_A0 RX interrupt */
