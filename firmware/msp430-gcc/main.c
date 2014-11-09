242f4c374dc43efdc293ef3dcd0c5c16f31acd7f
frankalicious <af.frankalicious@googlemail.com>
2014-11-09 13:27:19 +0100

    init msp430 clock
    To get a stable msp430 clock for uart initialize the clock.

--- firmware/msp430-gcc/main.c
+++ firmware/msp430-gcc/main.c
@@ -26,6 +26,8 @@ void uart_rx_isr(unsigned char c) {
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                  /* Stop WDT */
+  BCSCTL1 = CALBC1_1MHZ; /* Set range */
+  DCOCTL = CALDCO_1MHZ; /* SMCLK = DCO = 1MHz */
   uart_init();
 
   /* register ISR called when data was received */
